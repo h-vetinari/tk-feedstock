f124bd731b16cebadc1505a23b4c08ba297f6409
Michał Górny <mgorny@gentoo.org>
2026-07-17 13:35:34 +0200

    Replace hardwired exe suffixes in `build.bat`
    Signed-off-by: Michał Górny <mgorny@gentoo.org>

--- recipe/build.bat
+++ recipe/build.bat
@@ -60,11 +60,10 @@ nmake -f makefile.vc INSTALLDIR=%LIBRARY_PREFIX% %TCLSH_NATIVE% MACHINE=%MACHINE
 nmake -f makefile.vc INSTALLDIR=%LIBRARY_PREFIX% %TCLSH_NATIVE% MACHINE=%MACHINE% TCLDIR=..\..\tcl%PKG_VERSION% install
 if %ERRORLEVEL% GTR 0 exit 1
 
-:: Make sure that `wish` can be called without the version info.
-copy %LIBRARY_PREFIX%\bin\wish86t.exe %LIBRARY_PREFIX%\bin\wish.exe
-copy %LIBRARY_PREFIX%\bin\tclsh86t.exe %LIBRARY_PREFIX%\bin\tclsh.exe
+set VERSION_NODOT=%PKG_VERSION:.=%
+set MAJ_MIN=%VERSION_NODOT:~0,2%
 
-:: No `t` version of wish86.exe
-copy %LIBRARY_PREFIX%\bin\wish86t.exe %LIBRARY_PREFIX%\bin\wish86.exe
-copy %LIBRARY_PREFIX%\bin\tclsh86t.exe %LIBRARY_PREFIX%\bin\tclsh86.exe
+:: Make sure that `wish` can be called without the version info.
+copy %LIBRARY_PREFIX%\bin\wish%MAJ_MIN%.exe %LIBRARY_PREFIX%\bin\wish.exe
+copy %LIBRARY_PREFIX%\bin\tclsh%MAJ_MIN%.exe %LIBRARY_PREFIX%\bin\tclsh.exe
 popd
