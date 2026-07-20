77c24825823a2860ed06adb7b5e47f0735898825
Michał Górny <mgorny@gentoo.org>
2026-07-20 14:33:40 +0200

    Disable zipfs, unnecessary and breaks macOS code signing
    Signed-off-by: Michał Górny <mgorny@gentoo.org>

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,13 +7,20 @@ pushd tcl${PKG_VERSION}/unix
   # build and install a native interpreter first
   mkdir native
   cd native
-  ../configure --prefix="${BUILD_PREFIX}" --host="${BUILD}" \
-    CC="${CC_FOR_BUILD}" CFLAGS="-O3 -isystem ${BUILD_PREFIX}/include"
+  ../configure --prefix="${BUILD_PREFIX}"                     \
+               --host="${BUILD}"                              \
+               --disable-zipfs                                \
+               CC="${CC_FOR_BUILD}"                           \
+               CFLAGS="-O3 -isystem ${BUILD_PREFIX}/include"
   make -j${CPU_COUNT} ${VERBOSE_AT} install-binaries install-libraries
   cd ..
 
   # build the actual package
-  ./configure --prefix="${PREFIX}" --with-system-libtommath
+  # --enable-zipfs causes Tcl to embed its standard library in the exec
+  # this is unnecessary, and also breaks signing on macOS
+  ./configure --prefix="${PREFIX}"      \
+              --with-system-libtommath  \
+              --disable-zipfs
   make -j${CPU_COUNT} ${VERBOSE_AT}
 popd
 
@@ -26,8 +33,9 @@ fi
 
 pushd tk${PKG_VERSION}/unix
   # autoreconf -vfi
-  ./configure --prefix="${PREFIX}"        \
+  ./configure --prefix="${PREFIX}"                     \
               --with-tcl=../../tcl${PKG_VERSION}/unix  \
+              --disable-zipfs                          \
               ${CONFIGURE_ARGS}
   cat config.log
   make -j${CPU_COUNT} ${VERBOSE_AT}
